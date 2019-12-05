f92df46ddbfe577004930abfa8b9deecf8f611b2
Ronaldo <ronaldoverardo@gmail.com>
2019-12-04 22:49:43 -0200

    Primeiroas modificações

--- OpenArquivo/src/Main.java
+++ OpenArquivo/src/Main.java
@@ -1,40 +1,110 @@
 import java.io.File;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.Iterator;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+//@SuppressWarnings("unused")
 public class Main 
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException 
 	{
         JFileChooser escolhedor = new JFileChooser();
 
-        escolhedor.setFileFilter(new FileFilter() 
-        {
-            @Override
-            public String getDescription() 
-            {
-//              return "Somente arquivos Excel .XLSX ou .XLS"; //Exibe essa mensagem
-              return "*.xlsx"; //Exibe essa mensagem
-            }
-            
-            @Override
-            public boolean accept(File arq) 
-            {
-//              return (arq.getName().endsWith(".txt") || arq.isDirectory());
-//              return (arq.getName().endsWith(".XLSX ou .XLS") || arq.isDirectory());
-              return (arq.getName().endsWith("*.XLSX") || arq.isDirectory());
-            }
-        });
-
+        escolhedor.setFileFilter(new FileNameExtensionFilter("Microsoft Excel Files", "xls", "xlsx")); 
+        escolhedor.setAcceptAllFileFilterUsed(false);
+        
         int opcaoEscolhida = escolhedor.showOpenDialog(null); //Janela para abrir um arquivo
 
         if(opcaoEscolhida == JFileChooser.APPROVE_OPTION) 
         {
             //Código pra caso o usuário tenha escolhido abrir um arquivo
-        }
-		
-		
-	}
+        	
+//        	escolhedor.getSelectedFile().getName()
 
+        	// Mosta o nome do arquivo Excel lido
+        	System.out.println(escolhedor.getSelectedFile().getName());
+        	
+//          File excelFile = new File("contacts.xlsx");
+            File excelFile = new File(escolhedor.getSelectedFile().getName());
+
+            FileInputStream fis = new FileInputStream(excelFile);
+
+            // we create an XSSF Workbook object for our XLSX Excel File
+            XSSFWorkbook workbook = new XSSFWorkbook(fis);
+            // we get first sheet
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            // we iterate on rows
+            Iterator<Row> rowIt = sheet.iterator();
+
+            while(rowIt.hasNext()) 
+            {
+              Row row = rowIt.next();
+
+              // iterate on cells for the current row
+              Iterator<Cell> cellIterator = row.cellIterator();
+
+              while (cellIterator.hasNext()) 
+              {
+                Cell cell = cellIterator.next();
+                System.out.print(cell.toString() + ";");
+              }
+
+              System.out.println();
+            }
+
+            workbook.close();
+            fis.close();
+          }
+        	
+	}
 }
+
+
+
+
+
+//public class ReadExcel {
+//
+//  public static void main(String[] args) throws IOException {
+//    File excelFile = new File("contacts.xlsx");
+//    FileInputStream fis = new FileInputStream(excelFile);
+//
+//    // we create an XSSF Workbook object for our XLSX Excel File
+//    XSSFWorkbook workbook = new XSSFWorkbook(fis);
+//    // we get first sheet
+//    XSSFSheet sheet = workbook.getSheetAt(0);
+//
+//    // we iterate on rows
+//    Iterator<Row> rowIt = sheet.iterator();
+//
+//    while(rowIt.hasNext()) {
+//      Row row = rowIt.next();
+//
+//      // iterate on cells for the current row
+//      Iterator<Cell> cellIterator = row.cellIterator();
+//
+//      while (cellIterator.hasNext()) {
+//        Cell cell = cellIterator.next();
+//        System.out.print(cell.toString() + ";");
+//      }
+//
+//      System.out.println();
+//    }
+//
+//    workbook.close();
+//    fis.close();
+//  }
+//	
+//}
